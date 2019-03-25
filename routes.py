7b6806fd5a10e38904fe794ee8303f462ac7d6c9
taybur <taybur.rahaman@bjitgroup.com>
2019-03-25 05:59:52 -0400

    changed host ip to 0.0.0.0

--- routes.py
+++ routes.py
@@ -15,5 +15,5 @@ def hello():
     return 'Hello, World....'
 
 if __name__ == '__main__':
-  app.run(debug=True,host='192.168.112.3',port='5001') 
+  app.run(debug=True,host='0.0.0.0',port='5001') 
 
