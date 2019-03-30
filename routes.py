f5abbdfc3595463c6a8bd6a672d87b1e1b194c11
taybur <taybur.rahaman@bjitgroup.com>
2019-03-30 04:03:08 -0400

    Hello page added

--- routes.py
+++ routes.py
@@ -12,7 +12,7 @@ def about():
 
 @app.route('/hello')
 def hello():
-    return 'Hello, World....'
+  return render_template('hello.html')
 
 if __name__ == '__main__':
   app.run(debug=True,host='0.0.0.0',port='5001') 
